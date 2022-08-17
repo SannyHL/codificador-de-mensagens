fdde813728fa6133a380f24dfc968b20b67247e6
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-17 17:32:57 -0300

    projeto finalizado

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,30 @@ let textoDecodificado;
 let plaCodificar = document.getElementById('codificar');
 let plaDecodificar = document.getElementById('decodificar');
 
+let verifica = document.querySelector('#text-para-codificar');
+verifica.addEventListener('keypress', function(e){
+    
+    if(!verificaLetraMinusculas(e)){
+        e.preventDefault();
+    } 
+    
+})
+function verificaLetraMinusculas(e){
+    let char = String.fromCharCode(e.keyCode);
+
+    let analise = '[a-z]';
+    if(char.match(analise)){
+        return true;
+    }
+}
+
 
 function codificarTexto(){
+    
     entradaTexto = document.querySelector('#text-para-codificar').value;
-    entradaTexto = entradaTexto.toLowerCase();
     textoCodificado = entradaTexto.replace(/e/gm, "enter").replace(/i/gm, "imes").replace(/a/gm, "ai").replace(/o/gm, "ober").replace(/u/gm, "ufat");
     document.querySelector('#devolve-texto').innerHTML = textoCodificado;
-     
+    
 }
 
 console.log(codificarTexto())
@@ -25,6 +42,12 @@ async function copiaTexto(){
     let copia = document.getElementById('devolve-texto').value;
     await navigator.clipboard.writeText(copia);
 }
+
+document.getElementById("limpa").addEventListener('click', function limpaTexto(){
+    entradaTexto = "";
+});
+
+
 
 
 plaCodificar.onclick = codificarTexto;
