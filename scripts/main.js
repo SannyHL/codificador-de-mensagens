e8a23b268364ba1ba41b6fc5ddb8b875a26511a2
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-17 13:53:55 -0300

    Transformando letras maiusculas em minusculas

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,16 @@ let textoCodificado;
 let textoDecodificado;
 let plaCodificar = document.getElementById('codificar');
 let plaDecodificar = document.getElementById('decodificar');
-let textoCriptografado;
+
 
 function codificarTexto(){
-    
     entradaTexto = document.querySelector('#text-para-codificar').value;
+    entradaTexto = entradaTexto.toLowerCase();
     textoCodificado = entradaTexto.replace(/e/gm, "enter").replace(/i/gm, "imes").replace(/a/gm, "ai").replace(/o/gm, "ober").replace(/u/gm, "ufat");
     document.querySelector('#devolve-texto').innerHTML = textoCodificado;
      
 }
+
 console.log(codificarTexto())
 function decodificarTexto(){
     entradaTexto = document.querySelector('#text-para-codificar').value;
